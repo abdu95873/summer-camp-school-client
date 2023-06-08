b25d3d9dcd26e4b9246109dbadc55b139d250874
abdu95873 <abdu95873@gmail.com>
2023-06-08 13:37:00 +0600

    work on signup

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -3,20 +3,49 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Providers/AuthProviders';
 import Swal from 'sweetalert2';
 import axios from 'axios';
+import { appendErrors, useForm } from 'react-hook-form';
+
+
+
 
 const SignUp = () => {
 
 
-    const {createUser, userUpdate} = useContext(AuthContext);
+    const { createUser, userUpdate } = useContext(AuthContext);
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
 
 
-    const handleSignUp = event =>{
-        event.preventDefault();
-        const form = event.target;
-        const name = form.name.value;
-        const photo = form.photo.value;
-        const email = form.email.value;
-        const password = form.email.value;
+    // const handleSignUp = event =>{
+    //     event.preventDefault();
+    //     const form = event.target;
+    //     const name = form.name.value;
+    //     const photo = form.photo.value;
+    //     const email = form.email.value;
+    //     const password = form.email.value;
+
+    //     const user = {
+    //         name,
+    //         photo,
+    //         email,
+    //         password,
+    //         role: 'student'
+    //     }
+
+    //     createUser(email, password, name, photo)
+    //     .then(result =>{
+    //         saveUsers(user);            
+    //     } )
+    //     .catch(error => console.log(error))
+
+    // }
+
+    const handleSignUp = data =>{
+
+        
+        const name = data.name;
+        const photo = data.photoURL;
+        const email = data.email;
+        const password = data.password;
 
         const user = {
             name,
@@ -32,77 +61,146 @@ const SignUp = () => {
         } )
         .catch(error => console.log(error))
 
-    }
+    
+
+    };
+
+
+
 
 
     const saveUsers = (user) => {
         axios.post('http://localhost:5000/user', user, {
-          headers: {
-            'Content-Type': 'application/json'
-          }
+            headers: {
+                'Content-Type': 'application/json'
+            }
         })
-          .then(response => {
-            if (response.data.acknowledged) {
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'success',
-                    title: 'Your work has been saved',
-                    showConfirmButton: false,
-                    timer: 1500
-                  })
-              }
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      };
-    return (
+            .then(response => {
+                if (response.data.acknowledged) {
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Your work has been saved',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+
+    // return (
+        // <div className="hero min-h-screen bg-base-200">
+        //     <div className="hero-content flex-col lg:flex-row-reverse">
+
+        //         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        //             <form onSubmit={handleSignUp} className="card-body">
+        //                 <div className="form-control">
+        //                     <label className="label">
+        //                         <span className="label-text">Name</span>
+        //                     </label>
+        //                     <input type="text" name="name" placeholder="name" className="input input-bordered" />
+        //                 </div>
+        //                 <div className="form-control">
+        //                     <label className="label">
+        //                         <span className="label-text">Photo URL</span>
+        //                     </label>
+        //                     <input type="text" name="photo" placeholder="photo" className="input input-bordered" />
+        //                 </div>
+        //                 <div className="form-control">
+        //                     <label className="label">
+        //                         <span className="label-text">Email</span>
+        //                     </label>
+        //                     <input type="text" name="email" placeholder="email" className="input input-bordered" />
+        //                 </div>
+        //                 <div className="form-control">
+        //                     <label className="label">
+        //                         <span className="label-text">Password</span>
+        //                     </label>
+        //                     <input type="text" name="password" placeholder="password" className="input input-bordered" />
+        //                     <label className="label">
+        //                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+        //                     </label>
+        //                 </div>
+
+        //                 {/* todo: add confirm pass */}
+        //                 <div className="form-control mt-6">
+
+        //                     <input className="btn btn-primary" type="submit" value="Login"></input>
+        //                 </div>
+        //             </form>
+        //             <p className="my-4 text-center">
+        //                 Already have an account! <Link to="/login" className="text-red-600 my-4 text-center">Login</Link>
+        //             </p>
+        //         </div>
+        //     </div>
+        // </div>
+    // );
+// };
+
+return (
+    <>
+        
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
-               
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Sign up now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form onSubmit={handleSignUp} className="card-body">
-                    <div className="form-control">
+                    <form onSubmit={handleSubmit(handleSignUp)} className="card-body">
+                        <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
-                            <input type="text" name="name" placeholder="name" className="input input-bordered" />
+                            <input type="text"  {...register("name", { required: true })} name="name" placeholder="Name" className="input input-bordered" />
+                            {appendErrors.name && <span className="text-red-600">Name is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Photo URL</span>
                             </label>
-                            <input type="text" name="photo" placeholder="photo" className="input input-bordered" />
+                            <input type="text"  {...register("photoURL", { required: true })} placeholder="Photo URL" className="input input-bordered" />
+                            {errors.photoURL && <span className="text-red-600">Photo URL is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" name="email" placeholder="email" className="input input-bordered" />
+                            <input type="email"  {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
+                            {errors.email && <span className="text-red-600">Email is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" name="password" placeholder="password" className="input input-bordered" />
+                            <input type="password"  {...register("password", {
+                                required: true,
+                                minLength: 6,
+                                maxLength: 20,
+                                pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                            })} placeholder="password" className="input input-bordered" />
+                            {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                            {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+                            {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>}
+                            {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase one lower case, one number and one special character.</p>}
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
-
-                        {/* todo: add confirm pass */}
                         <div className="form-control mt-6">
-                            
-                            <input className="btn btn-primary" type="submit" value="Login"></input>
+                            <input className="btn btn-primary" type="submit" value="Sign Up" />
                         </div>
                     </form>
-                    <p className="my-4 text-center"> 
-                       Already have an account! <Link to="/login" className="text-red-600 my-4 text-center">Login</Link>
-                    </p>
+                    <p><small>Already have an account <Link to="/login">Login</Link></small></p>
+                    
                 </div>
             </div>
         </div>
-    );
+    </>
+);
 };
 
 export default SignUp;
