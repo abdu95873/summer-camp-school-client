1f9d3df33e6433bcdf5623e7598bc2b3ed45f263
abdu95873 <abdu95873@gmail.com>
2023-06-09 02:27:07 +0600

    work on admin

--- src/Pages/DashboardPages/AdminDashbordPages/ManageClasses.jsx
+++ src/Pages/DashboardPages/AdminDashbordPages/ManageClasses.jsx
@@ -16,24 +16,24 @@ const ManageClasses = () => {
     });
 
     useEffect(() => {
-                fetch(`http://localhost:5000/manage-class`, {
+        fetch(`http://localhost:5000/manage-class`, {
             method: 'PUT',
             headers: {
                 'content-type': 'application/json'
             },
             body: JSON.stringify(body)
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            if(data.modifiedCount>0){
-                refetch();
-            }
-        })
-      }, [body]);
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.modifiedCount > 0) {
+                    refetch();
+                }
+            })
+    }, [body]);
 
-    const handleStatus = async(btn, id, getStatus, getFeedback) => {
-        console.log(getFeedback,getStatus)
+    const handleStatus = async (btn, id, getStatus, getFeedback) => {
+        console.log(getFeedback, getStatus)
         if (btn == 'approve') {
             setBody({
                 status: 'approved',
@@ -63,7 +63,12 @@ const ManageClasses = () => {
             <table className="table">
                 <thead>
                     <tr>
+                        <th>Photo</th>
                         <th>Name</th>
+                        <th>Instructor name</th>
+                        <th>Instructor email</th>
+                        <th> Available seats</th>
+                        <th>Price</th>
                         <th>Status</th>
                         <th>Action</th>
                     </tr>
@@ -72,32 +77,37 @@ const ManageClasses = () => {
                     {
                         classes?.map(item => <>
                             <tr className="bg-base-200">
+                                <td><img className='h-16' src={item?.photo} alt="" /></td>
                                 <td>{item?.name}</td>
+                                <td>{item?.instructorName}</td>
+                                <td>{item?.instructorEmail}</td>
+                                <td>{item?.availablesseats}</td>
+                                <td className='text-end'>$ {item?.price}</td>
                                 <td>
                                     {item?.status == 'pending' ? 'Pending' : ''}
                                     {item?.status == 'approved' ? 'Approved' : ''}
                                     {item?.status == 'rejected' ? 'Rejected' : ''}
                                 </td>
                                 <td>
-                                    <button onClick={() => handleStatus('approve', item._id, item.status, item.feedback)}>Approve</button> <br />
-                                    <button onClick={() => handleStatus('reject', item._id, item.status, item.feedback)}>Reject</button> <br />
-                                    <button onClick={() => window[item._id].showModal()}>Feedback</button>
+                                    <button className="btn btn-success btn-xs" onClick={() => handleStatus('approve', item._id, item.status, item.feedback)}>Approve</button> <br />
+                                    <button className="btn btn-error btn-xs w-full my-2" onClick={() => handleStatus('reject', item._id, item.status, item.feedback)}>Reject</button> <br />
+                                    <button className="btn btn-info btn-xs" onClick={() => window[item._id].showModal()}>Feedback</button>
                                 </td>
                             </tr>
                             <dialog id={item._id} className="modal">
-  <form onSubmit={()=>handleStatus('feedback',item._id, item.status, item.feedback)} method="dialog" className="modal-box">
-  <div className="form-control">
-                <input type="text" name="feedback" placeholder="Feedback" className="input input-bordered" />
-            </div>
-            <div className="form-control mt-6">
+                                <form onSubmit={() => handleStatus('feedback', item._id, item.status, item.feedback)} method="dialog" className="modal-box">
+                                    <div className="form-control">
+                                        <input type="text" name="feedback" placeholder="Feedback" className="input input-bordered" />
+                                    </div>
+                                    <div className="form-control mt-6">
 
-<input className="btn btn-primary" type="submit" value="Add class"></input>
-</div>
-  </form>
-  <form method="dialog" className="modal-backdrop">
-    <button>close</button>
-  </form>
-</dialog>
+                                        <input className="btn btn-primary" type="submit" value="Add class"></input>
+                                    </div>
+                                </form>
+                                <form method="dialog" className="modal-backdrop">
+                                    <button>close</button>
+                                </form>
+                            </dialog>
                         </>)
 
                     }
--- src/Pages/DashboardPages/InstructorDashboardPages/MyClasses.jsx
+++ src/Pages/DashboardPages/InstructorDashboardPages/MyClasses.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const MyClasses = () => {
+    
     return (
         <div>
             My Classes
