588b3fdecdb46bfa4b2d942d7a3f2b29cc1f3e69
abdu95873 <abdu95873@gmail.com>
2023-06-12 11:59:44 +0600

    work on deploy

--- src/Pages/DashboardPages/InstructorDashboardPages/AddClasses.jsx
+++ src/Pages/DashboardPages/InstructorDashboardPages/AddClasses.jsx
@@ -1,14 +1,12 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../../../Providers/AuthProviders";
 import Swal from "sweetalert2";
 import axios from "axios";
-import { useQuery } from "react-query";
-import { useLocation, useNavigate } from "react-router-dom";
+
 
 const AddClasses = () => {
     const {user} = useContext(AuthContext);
-    const navigate = useNavigate();
-    const location = useLocation();
+    
 
 
     const handleAddClass = event => {
@@ -16,10 +14,12 @@ const AddClasses = () => {
         const form = event.target;
         const name = form.name.value;
         const photo = form.photo.value;
-        const availablesseats = form.availablesseats.value;
+        const availablesseats = form.availableSeats.value;
         const price = form.price.value;
         const instructorName = user?.displayName;
         const instructorEmail = user?.email;
+
+        console.log(name,photo, availablesseats, price, instructorEmail, instructorName);
 
         const body = {
             name, photo, availablesseats, price, instructorName, instructorEmail, status: 'pending', feedback: ''
@@ -39,12 +39,13 @@ const AddClasses = () => {
                       showConfirmButton: false,
                       timer: 1500
                     })
+                    form.reset();
                 }
             })
             .catch(error => {
               console.error(error);
             });
-
+            form.reset();
 
     }
    
@@ -56,7 +57,7 @@ const AddClasses = () => {
               <label className="label">
                 <span className="label-text">Class Name</span>
               </label>
-              <input type="text" name="name" placeholder="classname" className="input input-bordered" />
+              <input type="text" name="name" placeholder="name" className="input input-bordered" />
             </div>
             <div className="form-control">
               <label className="label">
@@ -77,7 +78,6 @@ const AddClasses = () => {
               <input type="number" name="price" placeholder="price" className="input input-bordered" />
             </div>
   
-            {/* todo: add confirm pass */}
             <div className="form-control mt-6">
               <input className="btn btn-primary" type="submit" value="Add class" />
             </div>
